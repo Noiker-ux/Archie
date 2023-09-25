340a07e1a103e16f4f0df8572a27981bb889a3a4
Alex Vyugin <pasha_pasha_201@mail.ru>
2023-09-25 14:29:39 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+$('.album__inner').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});
+$('.shop__inner').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});
